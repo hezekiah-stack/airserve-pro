fbd71c9ee30fb2250f15bc9503923ec8a7b88c7d
hezekiah-stack <hezekiahdomocol@gmail.com>
2026-05-18 11:17:33 +0800

    Add PATCH method to CORS configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ app.use(cors({
     process.env.FRONTEND_URL,
   ].filter(Boolean),
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+ methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
